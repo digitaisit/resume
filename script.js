49d1c6d8870f9e96e5a000fbc0caeb902ead0623
Michal Palko <Michal.Palko1@ibm.com>
2023-07-19 18:44:06 +0200

    fix

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
 
+// Check the user's theme preference from local storage
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme) {
+  body.classList.add(currentTheme);
+}
+
 themeToggle.addEventListener('change', () => {
-  body.classList.toggle('light-mode');
+  if (body.classList.contains('light-mode')) {
+    // If the current theme is light, switch to dark
+    body.classList.replace('light-mode', 'dark-mode');
+    localStorage.setItem('theme', 'dark-mode');
+  } else {
+    // If the current theme is dark, switch to light
+    body.classList.replace('dark-mode', 'light-mode');
+    localStorage.setItem('theme', 'light-mode');
+  }
 });
