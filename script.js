360342054c2fb60ffba6a5319c9b4edce66744cc
Michal Palko <Michal.Palko1@ibm.com>
2023-07-27 16:23:56 +0200

    for SO

--- script.js
+++ script.js
@@ -4,27 +4,25 @@ const body = document.body;
 const footer = document.querySelector('footer');
 const socialIconTwitter = document.querySelector('.social-icons-top .fab.fa-twitter');
 const socialIconLinkedin = document.querySelector('.social-icons-top .fab.fa-linkedin');
-const socialIconPornhub = document.getElementById('custom-icon');
 
 
 // Check the user's theme preference from local storage
 const currentTheme = localStorage.getItem('theme');
 if (currentTheme) {
   body.classList.add(currentTheme);
-  updateThemeLabel(currentTheme); // Update the theme label text and color
+  updateThemeLabel(currentTheme); 
 }
 
 themeToggle.addEventListener('change', () => {
+  
   if (body.classList.contains('light-mode')) {
     // If the current theme is light, switch to dark
     body.classList.replace('light-mode', 'dark-mode');
-    footer.style.backgroundColor = '#333'; // Set footer background color for dark mode
     updateThemeLabel('dark-mode'); // Update the theme label text and color
     localStorage.setItem('theme', 'dark-mode');
   } else {
     // If the current theme is dark, switch to light
     body.classList.replace('dark-mode', 'light-mode');
-    footer.style.backgroundColor = '#fff'; // Set footer background color for light mode
     updateThemeLabel('light-mode'); // Update the theme label text and color
     localStorage.setItem('theme', 'light-mode');
   }
@@ -33,26 +31,22 @@ themeToggle.addEventListener('change', () => {
 function updateThemeLabel(theme) {
   if (theme === 'dark-mode') {
     themeLabel.textContent = 'Burn your eyes';
-    themeLabel.style.color = '#fff'; // Set text color for dark theme
-    footer.style.color = '#fff'; // Set text color for dark theme in the footer
+    themeLabel.style.color = '#fff'; 
+    footer.style.color = '#fff'; 
+    footer.style.backgroundColor = '#333'; 
+    body.style.backgroundColor = '#333'; 
+    body.style.color = '#fff';
     socialIconTwitter.style.color = '#fff';
     socialIconLinkedin.style.color = '#fff';
-    // socialIconPornhub.style.color = '#fff';
-    socialIconPornhub.src = 'phicon.png';
-    // socialIconsTop.forEach(icon => {
-    //   icon.style.color = '#fff'; // Invert the color of the social icons for dark theme
-    // });
   } else {
     themeLabel.textContent = 'Quickly back!';
-    themeLabel.style.color = '#000'; // Set text color for light theme
-    footer.style.color = '#000'; // Set text color for light theme in the footer
+    themeLabel.style.color = '#000'; 
+    footer.style.color = '#000'; 
+    footer.style.backgroundColor = '#fff'; 
+    body.style.backgroundColor = '#fff';
+    body.style.color = '#000';
     socialIconTwitter.style.color = '#000';
     socialIconLinkedin.style.color = '#000';
-    // socialIconPornhub.style.color = '#000';
-    socialIconPornhub.src = 'phicon_wh.png';
-    // socialIconsTop.forEach(icon => {
-    //   icon.style.color = '#000'; // Reset the color of the social icons for light theme
-    // });
   }
 }
 
